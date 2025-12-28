13c6792c3c1f56823cc8475765d89608a77b5869
celiikerenn <eren1.celikk@gmail.com>
2025-12-28 21:49:02 +0300

    .

--- tracker/views.py
+++ tracker/views.py
@@ -128,7 +128,7 @@ from django.http import HttpResponse
 
 def create_admin(request):
     if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin','admin')
+        User.objects.create_superuser('admin', 'admin@example.com' ,'admin')
         return HttpResponse("Superuser 'admin' oluşturuldu! Şifre: admin")
     else:
         return HttpResponse("Bu kullanıcı zaten var.")
